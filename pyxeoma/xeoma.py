2b60ff5c42df4af543ed42298418af4e614ddcc4
Jerad Meisner <jerad.meisner@gmail.com>
2018-02-02 18:41:51 -0800

    fix for when admin/viewer credentials are different. fixes #1

--- pyxeoma/xeoma.py
+++ pyxeoma/xeoma.py
@@ -1,11 +1,12 @@
 import aiohttp
 import asyncio
+import base64
 import re
 
-from urllib.parse import quote
+from urllib.parse import quote, unquote
 
 class Xeoma():
-    def __init__(self, base_url, new_version, login=None, password=None):
+    def __init__(self, base_url, login=None, password=None):
         """
             Create a Xeoma Object for interacting with Xeoma web server
 
@@ -22,8 +23,6 @@ class Xeoma():
             self._base_url = base_url
         self._login = login
         self._password = password
-        self._cookie = None
-        self._new_version = new_version
 
     @asyncio.coroutine
     def async_test_connection(self):
@@ -37,24 +36,22 @@ class Xeoma():
             raise XeomaError('Received bad response from Xeoma server')
 
     @asyncio.coroutine
-    def async_get_camera_image(self, image_name):
+    def async_get_camera_image(self, image_name, username=None, password=None):
         """
             Grab a single image from the Xeoma web server
 
             Arguments:
                 image_name: the name of the image to fetch (i.e. image01)
+                username: the username to directly access this image
+                password: the password to directly access this image
         """
 
-        if self._cookie is None:
-            self._cookie = yield from self.get_session_cookie()
         try:
-            data = yield from self.async_fetch_image_data(image_name)
+            data = yield from self.async_fetch_image_data(
+                image_name, username, password)
             if data is None:
-                self._cookie = yield from self.get_session_cookie()
-                data = yield from self.async_fetch_image_data(image_name)
-                if data is None:
-                    raise XeomaError('Unable to authenticate with Xeoma web '
-                                     'server')
+                raise XeomaError('Unable to authenticate with Xeoma web '
+                                 'server')
             return data
         except asyncio.TimeoutError:
             raise XeomaError('Connection timeout while fetching camera image.')
@@ -62,17 +59,22 @@ class Xeoma():
             raise XeomaError('Unable to fetch image: {}'.format(e))
 
     @asyncio.coroutine
-    def async_fetch_image_data(self, image_name):
+    def async_fetch_image_data(self, image_name, username, password):
         """
             Fetch image data from the Xeoma web server
 
             Arguments:
                 image_name: the name of the image to fetch (i.e. image01)
+                username: the username to directly access this image
+                password: the password to directly access this image
         """
         params = {}
-        if not self._new_version:
-            params['login'] = ''
-        with aiohttp.ClientSession(cookies=self._cookie) as session:
+        cookies = self.get_session_cookie()
+        if username is not None and password is not None:
+            params['user'] = self.encode_user(username, password)
+        else:
+            params['user'] = ''
+        with aiohttp.ClientSession(cookies=cookies) as session:
             resp = yield from session.get(
                 '{}/{}.jpg'.format(self._base_url, image_name),
                 params=params
@@ -89,67 +91,52 @@ class Xeoma():
             Parse web server camera view for camera image names
         """
 
-        self._cookie = yield from self.get_session_cookie()
-        params = {}
+        cookies = self.get_session_cookie()
         try:
-            with aiohttp.ClientSession(cookies=self._cookie) as session:
+            with aiohttp.ClientSession(cookies=cookies) as session:
                 resp = yield from session.get(
-                    self._base_url,
-                    params=params
+                    self._base_url
                 )
                 t = yield from resp.text()
                 match = re.findall('(?:\w|\d)/(.*?).(?:mjpg|jpg)', t)
                 if len(match) == 0:
                     raise XeomaError('Unable to find any camera image names')
-                return set(match)
+                image_names = set(match)
+                results = []
+                for image_name in image_names:
+                    match = re.search(
+                        image_name + '\.(?:mjpg|jpg).*?user=(.*?)&', t
+                    )
+                    if match and len(match.group(1)) > 0:
+                        d = base64.b64decode(unquote(match.group(1))) \
+                            .decode('ASCII')
+                        creds = d.split(':')
+                        if len(creds) < 2:
+                            raise XeomaError('Error parsing image credentials')
+                        results.append((image_name, creds[0], creds[1]))
+                    else:
+                        results.append((image_name, None, None))
+                return results
         except asyncio.TimeoutError as e:
             raise XeomaError("Unable to connect to Xeoma web server")
 
-    @asyncio.coroutine
-    def async_get_session_key(self):
-        """
-            Retrieve the Xeoma web server sessionkey
-        """
+    def encode_user(self, username, password):
+        credentials = '{}:{}'.format(username, password)
+        b = base64.b64encode(bytes(credentials, encoding='ASCII'))
+        return quote(b.decode('ASCII'))
 
-        try:
-            with aiohttp.ClientSession() as session:
-                params = {'getsessionkey': ' '}
-                if self._login is not None:
-                    params['login'] = self._login
-                if self._password is not None:
-                    params['password'] = self._password
-
-                resp = yield from session.get(
-                    self._base_url,
-                    params=params,
-                    allow_redirects=False
-                )
-                t = yield from resp.text()
-                match = re.search('\'sessionkey\', "(.*)"', t)
-                if match:
-                    session_key = match.group(1)
-                    return session_key
-                else:
-                    return None
-        except asyncio.TimeoutError:
-            raise XeomaError('Connection timeout while fetching session key.')
-        except aiohttp.ClientError as e:
-            raise XeomaError('Unable to obtain session key: {}.'.format(e))
-
-    @asyncio.coroutine
     def get_session_cookie(self):
         """
             Create a session cookie object for use by aiohttp
         """
 
-        cookies = {}
         if self._login is not None and self._password is not None:
-            session_key = yield from self.async_get_session_key()
-            if session_key is None:
-                raise XeomaError('Unable to obtain session key.')
-            cookies['sessionkey'] = quote(session_key)
-        return cookies
+            session_key = self.encode_user(self._login, self._password)
+            return {'sessionkey': session_key}
+        else:
+            return None
 
 class XeomaError(Exception):
     def __init__(self, message):
         self.message = message
+
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pyxeoma',
     packages=['pyxeoma'],
-    version='1.2',  # Ideally should be same as your GitHub release tag version
+    version='1.3',
     description='Python wrapper for Xeoma web server API',
     author='Jerad Meisner',
     author_email='jerad.meisner@gmail.com',
     url='https://github.com/jeradM/pyxeoma',
-    download_url='https://github.com/jeradM/pyxeoma/archive/1.2.tar.gz',
+    download_url='https://github.com/jeradM/pyxeoma/archive/1.3.tar.gz',
     keywords=['Xeoma'],
     install_requires=[
         'aiohttp'
